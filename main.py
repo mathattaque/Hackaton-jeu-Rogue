7ec46b99e03b7e099921b357c5b9d09dc9a7acea
Mathis Grillot <mathissogri@gmail.com>
2024-02-02 11:02:26 +0100

    dessine le héros

--- main.py
+++ main.py
@@ -8,10 +8,8 @@ NB_CASES = TAILLE_FENETRE // TAILLE_CASE
 
 FPS=30
 
-
 class Hero:
-    def __init__(self, name, health, attack, x, y, color, score):
-        self.name = name
+    def __init__(self, health, attack, x, y, color):
         self.health = health
         self.attack = attack
         self.x = x
@@ -20,8 +18,7 @@ class Hero:
         self.color = color
 
     def draw(self, screen):
-        ellipse_rect = pg.Rect(self.x - TAILLE_CASE, self.y + TAILLE_CASE, 50, 30)
-        pg.draw.ellipse(screen, self.color, ellipse_rect)
+        pg.draw.rect(screen, self.color, (self.x*TAILLE_CASE,self.y*TAILLE_CASE,TAILLE_CASE,TAILLE_CASE))
 
     def is_dead(self):
         if self.health <= 0:
@@ -44,8 +41,6 @@ def display(screen,board):
             if board[i][j]==1:
                 pg.draw.rect(screen,(0,0,0),(i*TAILLE_CASE,j*TAILLE_CASE,TAILLE_CASE,TAILLE_CASE))
 
-    
-
 
 def main():
 
@@ -53,11 +48,11 @@ def main():
     ex_board=np.zeros((NB_CASES,NB_CASES))
     ex_board[1:NB_CASES-1,1:NB_CASES-1]=np.ones((NB_CASES-2,NB_CASES-2))
     #print(ex_board)
+    pg.init()
 
     pg.font.init()
     police = pg.font.Font(None, 36)  # Choisissez une taille de police qui convient
 
-    pg.init()
     screen = pg.display.set_mode((TAILLE_FENETRE,TAILLE_FENETRE))
     pg.display.set_caption('Rogue')
     clock=pg.time.Clock()
@@ -66,7 +61,11 @@ def main():
 
     score = 0
 
+    hero = Hero(100, 10, 400, 400, (255, 0, 0))
+
     while running:
+        clock.tick(FPS)
+
         for event in pg.event.get():
             if event.type==pg.QUIT:
                 running=False
@@ -74,13 +73,19 @@ def main():
             if event.type==pg.KEYDOWN:
                 if event.key==pg.K_q:
                     running=False
+
+        screen.fill((0, 0, 0))
         display(screen,ex_board)
 
+        hero.draw(screen)
+        
+        texte_score = police.render("Score : " + str(score), True, (125, 125, 125))
+        position_score = (10, 10) 
+        screen.blit(texte_score, position_score)
+
         pg.display.update()
-        clock.tick(FPS)
-    texte_score = police.render("Score : " + str(score), True, (255, 255, 255))
-    position_score = (10, 10) 
-    screen.blit(texte_score, position_score)
+
+    pg.quit()
 
 if __name__ == "__main__":
     main()
